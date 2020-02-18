8d712ef2ceb1adb3dac48e8da3015879374f6a05
lauraagss <61144271+lauraagss@users.noreply.github.com>
2020-02-18 10:49:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 	require_once('./includes/header.php');
 ?>
 	<main>
-		<h1>Accueil</h1>
-		<p>Lorem ipsum dolor sit amet, laura gonçalves. Sint vitae sit adipisci quos possimus, eveniet odit quisquam? Porro molestias modi impedit nesciunt similique dolorum, deleniti atque quas temporibus ut accusantium!</p>
+		<h1>Bienvenue sur l'accueil</h1>
+		<p>Lorem ipsum dolor sit amet, laura gonçalves.int vitae sit adipisci quos possimus S, eveniet odit quisquam? Porro molestias modi impedit nesciunt similique dolorum, deleniti atque quas temporibus ut accusantium!</p>
 	</main>
 
 <?php require_once('./includes/footer.php'); ?>
